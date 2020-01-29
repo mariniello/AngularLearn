ec954d82747c59df1279668f8d547350ce376751
mariniello <mariniello@hotmail.com>
2020-01-29 11:16:45 -0300

    Variável de referência

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FundamentosAngular';
+
+  nome = 'Thiago';
+
+  adicionar(nomeInput: any) {
+    this.nome = nomeInput;
+  }
 }
